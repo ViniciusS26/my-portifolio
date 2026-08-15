1e5a669d5d006fdfb63f475d625e514d7954e4ab
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2026-08-14 23:53:25 -0300

    fix:corrigindo erro de importação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 
 import './App.css'
 import Header from './header/Header.tsx'
+import Home from './home/Home.tsx'
+
 function App() {
   
 
   return (
     <>
       <Header />
+      <Home />
     </>
   )
 }
